c8af69851908a76478a7525edf22e0ad8705cc8a
Nana Bingies <nanabingies1@gmail.com>
2025-02-16 20:15:34 +0000

    Update 57_Insert_Interval.py

--- 57_Insert_Interval.py
+++ 57_Insert_Interval.py
@@ -11,6 +11,13 @@ class Solution(object):
         
         index = 0
         for interval in intervals:
+            if index + 1 >= len(intervals):
+                if newInterval[0] > interval[1]:
+                    intervals.append(newInterval)
+                else:
+                    intervals[index][0] = min(intervals[index][0], newInterval[0])
+                    intervals[index][1] = max(intervals[index][1], newInterval[1])
+                break
             if newInterval[0] > interval[0] and newInterval[0] < intervals[index+1][0]:
                 intervals[index][1] = newInterval[1]
                 if intervals[index][1] > intervals[index+1][0]:
@@ -24,6 +31,21 @@ class Solution(object):
     
 if __name__ == "__main__":
     soln = Solution()
+    
+    intervals = [[1,5]]
+    newInterval = [6,8]
+    output = soln.insert(intervals, newInterval)
+    print(f"list : {output}")
+    
+    intervals = [[1,5]]
+    newInterval = [2,7]
+    output = soln.insert(intervals, newInterval)
+    print(f"list : {output}")
+    
+    intervals = [[1,5]]
+    newInterval = [2,3]
+    output = soln.insert(intervals, newInterval)
+    print(f"list : {output}")
     
     intervals = [[1,3],[6,9]]
     newInterval = [2,5]
