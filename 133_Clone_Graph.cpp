f1c4e1b3141f2dd3cf884808e5df2c0dc9c0b2b9
Nana Bingies <nanabingies1@gmail.com>
2025-10-01 12:44:13 +0200

    Create 133_Clone_Graph.cpp

---
+++ 133_Clone_Graph.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        int val = node->val;
+        vector<Node*> neighbors = node->neighbors;
+    }
+};
+
+int main() {
+    Solution soln = Solution();
+}
